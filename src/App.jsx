e5ad2dc5878f2130c0e4deda4d739e12ca31c8b2
Wasif Jameel <wasif.jameel1@gmail.com>
2025-02-27 11:30:35 +0500

    initial commit with calling feature

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css';
+import JoinForm from './JoinForm';
+// import Header from './Header';
+import Footer from './Footer';
+import Conference from './Conference';
+import { useEffect } from 'react';
+import {
+  selectIsConnectedToRoom,
+  useHMSActions,
+  useHMSStore,
+} from '@100mslive/react-sdk';
+
+export default function App() {
+  const isConnected = useHMSStore(selectIsConnectedToRoom);
+  const hmsActions = useHMSActions();
+
+  useEffect(() => {
+    window.onunload = () => {
+      if (isConnected) {
+        hmsActions.leave();
+      }
+    };
+  }, [hmsActions, isConnected]);
+
+  return (
+    <div className="App">
+      {/* <Header /> */}
+      {isConnected ? (
+        <>
+          <Conference />
+          <Footer />
+        </>
+      ) : (
+        <JoinForm />
+      )}
+    </div>
+  );
+}
